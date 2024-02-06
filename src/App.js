f5cc5e6e3a7a78399b56445a0500e4e4a04f72e5
Piyush <piyushpatil4270@gmail.com>
2024-02-06 19:19:20 +0530

    commit-2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Pathfinder } from './Pathfinder/Pathfinder';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App w-full">
       <Pathfinder></Pathfinder>
     </div>
   );
